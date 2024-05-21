04581a5bb80cd3355674f2d4e322d22b9850cfe8
thorbjoernl (Thorbjørn) <thorbjorn.s.lundin@met.no>
2024-05-21 11:01:24 +0000

    feat: /glob_stats endpoint

--- src/aerovaldb/jsonfiledb.py
+++ src/aerovaldb/jsonfiledb.py
@@ -1,12 +1,18 @@
 from .aerovaldb import AerovalDB
+from pathlib import Path
 import logging
+from aerovaldb.aerovaldb import get_method, put_method
+import os
+import json
 
 logger = logging.getLogger(__name__)
 
 
 class AerovalJsonFileDB(AerovalDB):
-    def __init__(self, basedir):
+    def __init__(self, basedir: str | Path):
         self._basedir = basedir
+        if isinstance(self._basedir, str):
+            self._basedir = Path(self._basedir)
 
     def _get(self, route, route_args, *args, **kwargs):
         logger.debug(f"_get({route}, {route_args}, {args}, {kwargs})")
@@ -17,3 +23,9 @@ class AerovalJsonFileDB(AerovalDB):
         logger.debug(f"_put({obj}, {route}, {route_args}, {args}, {kwargs})")
         print("writing to ", route.format(**route_args), ".json")
         return "dummyPut"
+
+    @get_method("/glob_stats/{project}/{experiment}/{frequency}")
+    def get_glob_stats(self, project: str, experiment: str, frequency: str):
+        file_path = os.path.join(self._basedir, project, experiment, "hm", f'glob_stats_{frequency}.json')
+        with open(file_path) as f:
+            return json.load(f)
