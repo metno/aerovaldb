e8a9ab60dea6c4567664fac40103df4c9788d857
thorbjoernl (Thorbjørn) <thorbjorn.s.lundin@met.no>
2024-05-22 10:02:51 +0000

    FIX

--- src/aerovaldb/jsonfiledb.py
+++ src/aerovaldb/jsonfiledb.py
@@ -5,6 +5,9 @@ from aerovaldb.aerovaldb import get_method, put_method
 import os
 import json
 import orjson
+from enum import Enum
+
+AccessType = Enum("AccessType", ["JSON_STR", "FILE_PATH", "OBJ"])
 
 logger = logging.getLogger(__name__)
 
@@ -26,11 +29,23 @@ class AerovalJsonFileDB(AerovalDB):
 
         relative_path = file_path_template.format(**route_args)
 
-        return os.path.join(self._basedir, relative_path)
+        return Path(os.path.join(self._basedir, relative_path)).resolve()
 
     def _get(self, route, route_args, *args, **kwargs):
+        access_type = kwargs.get("type", AccessType.JSON_STR)
+
         file_path = self._get_file_path_from_route(route, route_args)
+
+        if access_type == AccessType.FILE_PATH:
+            return str(file_path)
+
         logger.debug(f"Mapped route {route} / { route_args} to file {file_path}.")
+
+        if access_type == AccessType.JSON_STR:
+            with open(file_path, "rb") as f:
+                json = str(f.read())
+
+            return json
 
         with open(file_path, "rb") as f:
             raw = f.read()
