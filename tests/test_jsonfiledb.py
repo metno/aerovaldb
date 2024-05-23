c580579ba12f169cf9c18c8ec992e72060bac568
thorbjoernl (Thorbjørn) <thorbjorn.s.lundin@met.no>
2024-05-23 14:20:36 +0000

    test: New getter test for get_glob_stats, get_contour, get_ts and get_ts_weekly

--- tests/test_jsonfiledb.py
+++ tests/test_jsonfiledb.py
@@ -1,12 +1,45 @@
 import pytest
 import aerovaldb
 
+
+@pytest.mark.parametrize("resource", (("json_files:./tests/test-db/json",)))
+@pytest.mark.parametrize(
+    "fun,args,expected",
+    (
+        (
+            "get_glob_stats",
+            ["project", "experiment", "frequency"],
+            "./project/experiment/hm/",
+        ),
+        ("get_contour", ["project", "experiment"], "./project/experiment/contour/"),
+        (
+            "get_ts",
+            ["project", "experiment", "region", "network", "obsvar", "layer"],
+            "./project/experiment/ts/",
+        ),
+        (
+            "get_ts_weekly",
+            ["project", "experiment", "region", "network", "obsvar", "layer"],
+            "./project/experiment/ts/dirunal/",
+        ),
+    ),
+)
+def test_getter(resource: str, fun: str, args: list, expected):
+    with aerovaldb.open(resource) as db:
+        fun = getattr(db, fun)
+
+        data = fun(*args)
+
+        assert data["path"] == expected
+
+
 def test_get_glob_stats():
     with aerovaldb.open(f"json_files:./tests/test-db/json") as db:
         data = db.get_glob_stats(
             "project", "experiment", "frequency", access_type="OBJ"
         )
         assert data["path"] == "./project/experiment/hm/"
+
 
 def test_put_glob_stats():
     # TODO: These tests should ideally cleanup after themselves. For now
@@ -16,13 +49,15 @@ def test_put_glob_stats():
         obj = {"data": "gibberish"}
         db.put_glob_stats(obj, "test1", "test2", "test3")
         read_data = db.get_glob_stats("test1", "test2", "test3")
-        
+
         assert obj["data"] == read_data["data"]
+
 
 def test_get_contour():
     with aerovaldb.open(f"json_files:./tests/test-db/json") as db:
         data = db.get_contour("project", "experiment")
         assert data["path"] == "./project/experiment/contour/"
+
 
 def test_put_contour():
     with aerovaldb.open("json_files:./tests/test-db/tmp") as db:
@@ -31,6 +66,7 @@ def test_put_contour():
         read_data = db.get_contour("test1", "test2", "test3", "test4")
         assert obj["data"] == read_data["data"]
 
+
 def test_get_ts():
     with aerovaldb.open(f"json_files:./tests/test-db/json") as db:
         data = db.get_ts(
@@ -38,6 +74,7 @@ def test_get_ts():
         )
 
         assert data["path"] == "./project/experiment/ts/"
+
 
 def test_put_ts():
     with aerovaldb.open("json_files:./tests/test-db/tmp") as db:
