1261c593b1dd60c90d49cc3181e4244fdab88c7b
thorbjoernl (Thorbjørn) <thorbjorn.s.lundin@met.no>
2024-05-23 11:21:11 +0000

    test: Switch to pytest

---
+++ tests/test_jsonfiledb.py
@@ -0,0 +1,49 @@
+import pytest
+import aerovaldb
+
+def test_get_glob_stats():
+    with aerovaldb.open(f"json_files:./tests/test-db/json") as db:
+        data = db.get_glob_stats(
+            "project", "experiment", "frequency", access_type="OBJ"
+        )
+        assert data["path"] == "./project/experiment/hm/"
+
+def test_put_glob_stats():
+    # TODO: These tests should ideally cleanup after themselves. For now
+    # it is best to delete ./tests/test-db/tmp before running to verify
+    # that they run as intended.
+    with aerovaldb.open("json_files:./tests/test-db/tmp") as db:
+        obj = {"data": "gibberish"}
+        db.put_glob_stats(obj, "test1", "test2", "test3")
+        read_data = db.get_glob_stats("test1", "test2", "test3")
+        
+        assert obj["data"] == read_data["data"]
+
+def test_get_contour():
+    with aerovaldb.open(f"json_files:./tests/test-db/json") as db:
+        data = db.get_contour("project", "experiment")
+        assert data["path"] == "./project/experiment/contour/"
+
+def test_put_contour():
+    with aerovaldb.open("json_files:./tests/test-db/tmp") as db:
+        obj = {"data": "gibberish"}
+        db.put_contour(obj, "test1", "test2", "test3", "test4")
+        read_data = db.get_contour("test1", "test2", "test3", "test4")
+        assert obj["data"] == read_data["data"]
+
+def test_get_ts():
+    with aerovaldb.open(f"json_files:./tests/test-db/json") as db:
+        data = db.get_ts(
+            "project", "experiment", "region", "network", "obsvar", "layer"
+        )
+
+        assert data["path"] == "./project/experiment/ts/"
+
+def test_put_ts():
+    with aerovaldb.open("json_files:./tests/test-db/tmp") as db:
+        obj = {"data": "gibberish"}
+        db.put_ts(obj, "test1", "test2", "test3", "test4", "test5", "test6")
+
+        read_data = db.get_ts("test1", "test2", "test3", "test4", "test5", "test6")
+
+        assert obj["data"] == read_data["data"]
