85b0489f56a8ff69036b750a9cde1e008291c913
Thorbjørn Lundin <thorbjorn.s.lundin@met.no>
2024-05-24 14:05:21 +0200

    fix: Wrong path template for /reports

--- src/aerovaldb/jsonfiledb.py
+++ src/aerovaldb/jsonfiledb.py
@@ -45,7 +45,7 @@ class AerovalJsonFileDB(AerovalDB):
             "/v0/hm_ts/{project}/{experiment}/{station}/{network}/{obsvar}/{layer}": "./{project}/{experiment}/hm/ts/{station}-{network}-{obsvar}-{layer}.json",
             "/v0/forecast/{project}/{experiment}/{station}/{network}/{obsvar}/{layer}": "./{project}/{experiment}/forecast/{station}_{network}-{obsvar}_{layer}.json",
             "/v0/gridded_map/{project}/{experiment}/{obsvar}/{model}": "./{project}/{experiment}/contour/{obsvar}_{model}.json",
-            "/v0/report/{project}/{experiment}/{title}": "./reports/{project}/{experiment}/contour/{title}.json",
+            "/v0/report/{project}/{experiment}/{title}": "./reports/{project}/{experiment}/{title}.json",
         }
 
     def _normalize_access_type(
