e9ea0bdd98d8528d4ba883557a29e632968e0dcf
thorbjoernl <thorbjorn.s.lundin@met.no>
2024-05-29 10:23:09 +0200

    feat: Add UnusedArguments

--- src/aerovaldb/exceptions.py
+++ src/aerovaldb/exceptions.py
@@ -3,3 +3,9 @@ class FileDoesNotExist(IOError):
     Exception raised by jsondb in FILE_PATH mode, if the resulting file
     does not exist.
     """
+
+class UnusedArguments(ValueError):
+    """
+    Raised by jsondb if args or kwargs remain after matching (which
+    is likely a mistake by the caller). 
+    """
