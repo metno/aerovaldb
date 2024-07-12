5c8c23f8aa9018082a925af3a9e282773082dd93
thorbjoernl <thorbjorn.s.lundin@met.no>
2024-07-12 14:23:04 +0200

    doc: Document access_type

--- src/aerovaldb/types.py
+++ src/aerovaldb/types.py
@@ -1,3 +1,16 @@
-from enum import Enum
+from enum import Enum, auto
 
-AccessType = Enum("AccessType", ["JSON_STR", "FILE_PATH", "OBJ"])
+
+class AccessType(Enum):
+    """Enumeration of access types. Specifies how data will be read
+    and returned.
+
+    JSON_STR: Result will be returned as an unparsed json string.
+    FILE_PATH: Result will be returned as the file path to the file
+    containing the data.
+    OBJ: The json will be parsed and returned as a python object.
+    """
+
+    JSON_STR = auto()
+    FILE_PATH = auto()
+    OBJ = auto()
