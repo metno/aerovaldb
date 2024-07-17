e52c7eb21a42d09797641f94aa9aa700681ab249
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-07-17 10:02:55 +0200

    Remove files

--- test.py
+++
@@ -1,8 +0,0 @@
-import aerovaldb
-
-with aerovaldb.open("json_files:.") as db:
-    db.acquire_lock()
-    data = db.get_by_uuid("./file.json", default={"counter": 0})
-    data["counter"] += 1
-    db.put_by_uuid(data, "./file.json")
-    db.release_lock()
