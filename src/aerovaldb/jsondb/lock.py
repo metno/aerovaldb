26150236e536f7854e9315853d7f38721720cc60
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-07-17 12:59:06 +0200

    fix

--- src/aerovaldb/jsondb/lock.py
+++ src/aerovaldb/jsondb/lock.py
@@ -10,8 +10,10 @@ class JsonDbLock:
     def __init__(self, lock_file: str | pathlib.Path, uuid: str):
         self._uuid = uuid
         logger.debug("Initializing lock with lockfile %s", lock_file)
-        self._lock_file = open(lock_file, "w")
+        self._lock_file = lock_file
+        self._lock_handle = open(lock_file, "a+")
         self._aiolock = asyncio.Lock()
+        self._acquired = False
 
     async def __aenter__(self):
         await self.acquire()
@@ -24,10 +26,15 @@ class JsonDbLock:
         logger.debug("Acquiring lock with lockfile %s", self._lock_file)
         await self._aiolock.acquire()
 
-        fcntl.lockf(self._lock_file, fcntl.LOCK_EX)
+        fcntl.lockf(self._lock_handle, fcntl.LOCK_EX)
+        self._acquired = True
 
     def release(self):
         logger.debug("Releasing lock with lockfile %s", self._lock_file)
 
-        fcntl.fcntl(self._lock_file, fcntl.LOCK_UN)
+        fcntl.fcntl(self._lock_handle, fcntl.LOCK_UN)
+        self._acquired = False
         self._aiolock.release()
+
+    def is_locked(self) -> bool:
+        return self._acquired
