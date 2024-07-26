13372921442ae91592772fbda62f7c270b7defd6
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-07-26 09:22:24 +0200

    test: Test open()

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,9 +1,23 @@
 import pytest
+import os
 
 import aerovaldb
+from aerovaldb.jsondb.jsonfiledb import AerovalJsonFileDB
 
 
 def test_plugins():
     engines = aerovaldb.list_engines()
     print(engines)
     assert len(engines) >= 1
+
+
+def test_open_1():
+    with aerovaldb.open("json_files:.") as db:
+        assert isinstance(db, AerovalJsonFileDB)
+        assert os.path.realpath(db._basedir) == os.path.realpath(".")
+
+
+def test_open_2():
+    with aerovaldb.open(".") as db:
+        assert isinstance(db, AerovalJsonFileDB)
+        assert os.path.realpath(db._basedir) == os.path.realpath(".")
