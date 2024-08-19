079ef1513df5a18ee431fd2611722ff90ddef6b2
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-08-19 16:38:15 +0200

    Skip log lines

--- mutmut_config.py
+++ mutmut_config.py
@@ -6,6 +6,9 @@ FILES_TO_SKIP = [
 
 
 def pre_mutation(context):
-    print(context.filename)
+    print(f"{context.filename}\n")
+    line = context.current_source_line.strip()
+    if line.startswith("logger."):
+        context.skip = True
     if context.filename in FILES_TO_SKIP:
         context.skip = True
