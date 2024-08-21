b28236d715d031112ee453454c0df8094f263d12
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-08-21 11:20:01 +0200

    Add benchmark script

---
+++ scripts/iterate_over_uris_benchmark.py
@@ -0,0 +1,21 @@
+import aerovaldb
+import time
+
+
+with aerovaldb.open("json_files:./tests/test-db/json") as db:
+    start_time = time.perf_counter()
+    count = len(list(db.list_all()))
+    end_time = time.perf_counter()
+
+print(f"json_files: {count} items in {end_time-start_time:.3f} seconds")
+
+# 0.197 seconds
+
+with aerovaldb.open("sqlitedb:./tests/test-db/sqlite/test.sqlite") as db:
+    start_time = time.perf_counter()
+    count = len(list(db.list_all()))
+    end_time = time.perf_counter()
+
+print(f"sqlite:     {count} items in {end_time-start_time:.3f} seconds")
+
+# 0.001 seconds
