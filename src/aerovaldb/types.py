06a0a5b1ded4c952a5933e55379402944021bd43
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2025-02-10 10:23:28 +0100

    fix: AccessType list formatting

--- src/aerovaldb/types.py
+++ src/aerovaldb/types.py
@@ -5,20 +5,21 @@ class AccessType(Enum):
     """Enumeration of access types. Specifies how data will be read
     and returned.
 
-    JSON_STR: Result will be returned as an unparsed json string.
+    * JSON_STR: Result will be returned as an unparsed json string.
 
-    FILE_PATH: Result will be returned as the file path to the file
+    * FILE_PATH: Result will be returned as the file path to the file
     containing the data.
 
-    OBJ: The json will be parsed and returned as a python object.
+    * OBJ: The json will be parsed and returned as a python object.
 
-    URI: A string which is a unique identifier of this asset between
-    implementations of Aerovaldb. Can be used with `get_by_uuid()` and
-    `put_by_uuid()` to read or write respectively.
-    (_ROW_ID: For Internal use)
-    MTIME: The timestamp for last modification for the resource will be
+    * URI: A string which is a unique identifier of this asset between
+    implementations of Aerovaldb. Can be used with :meth:`aerovaldb.AerovalDB.get_by_uuid`
+    and :meth:`aerovaldb.AerovalDB.put_by_uuid` to read or write respectively.
+
+    * MTIME: The timestamp for last modification for the resource will be
     returned (as datetime.datetime).
-    CTIME: The creation timestamp for the resource will be returned (as
+
+    * CTIME: The creation timestamp for the resource will be returned (as
     datetime.datetime)
     """
 
