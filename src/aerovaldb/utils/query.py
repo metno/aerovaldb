92701c104648e9a67c4eb64b9fabab59f1955e8f
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2025-03-05 14:39:57 +0100

    Documentation for queryEntry

--- src/aerovaldb/utils/query.py
+++ src/aerovaldb/utils/query.py
@@ -2,6 +2,12 @@ from ..types import AssetType
 
 
 class QueryEntry(str):
+    """Class representing information about an asset. Information
+    is represented as a string representing the URI of the asset
+    but additional information is available, namely type (See
+    AssetType) and meta (dict containing metadata about the asset).
+    """
+
     def __new__(cls, value: str, type: AssetType, meta: dict[str, str]):
         if not isinstance(value, str):
             raise TypeError(f"Provided value must be str, got {type(value)}.")
@@ -15,8 +21,14 @@ class QueryEntry(str):
 
     @property
     def type(self) -> AssetType:
+        """The type of the asset this represents (see AssetType for details)."""
         return self._type
 
     @property
     def meta(self) -> dict[str, str]:
+        """Dictionary containing metadata about the asset. Available keys should
+        reflect the args passed to put for this assettype.
+
+        :return: Dict containing metadata.
+        """
         return self._meta
